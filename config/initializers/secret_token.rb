f4ae55edc3322bf9bd863a860f9c8475bae60965
priyankodey <priyankodey87@gmail.com>
2014-08-29 11:22:50 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flambiance::Application.config.secret_key_base = '38eea876f19003dca9300f261ac72c008c2367fb9772e18744274b4e20f7d720af89b15556cc67df2379b173aa789c991bf9c8877a74add4d158ffe25d66b058'
